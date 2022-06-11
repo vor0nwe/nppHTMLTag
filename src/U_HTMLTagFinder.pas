vor0nwe/nppHTMLTag

commit 4fc5a0b78028f5be66932dc44433952dff07ca49

Fix memory leak
The JS decoding procedure was allocating a new TTextRange object
for *every* Unicode escape it found; none of them could be freed
because the object reference was replaced with each new match,
making the previous one inaccessible

This refactors TActiveDocument.Find to simply mutate a passed-in
object ref. The changes spilled over into the tag finder, but the
result is better memory efficiency, and (we'll see) perhaps even
better performance

src/U_HTMLTagFinder.pas
@@ -15,6 +15,7 @@ uses
 
 type
   TDirectionEnum = (dirBackward = -1, dirNone = 0, dirForward = 1, dirUnknown = 2);
+  TTextRange = NppSimpleObjects.TTextRange;
 
 const
   ncHighlightTimeout = 1000;
@@ -38,6 +39,8 @@ var
   ExtraChar: AnsiChar;
 begin
   ATagName := '';
+  TagEnd := TTextRange.Create(AView);
+  Result := TTextRange.Create(AView);
 
   if (APosition < 0) then begin
     if (AView.CurrentPosition <= AView.Selection.Anchor) then begin
@@ -46,11 +49,10 @@ begin
       APosition := AView.CurrentPosition;
     end;
   end;
-  Result := AView.Find('<', 0, APosition, 0);
-  if Result = nil then begin
-//    DebugWrite('ExtractTagName', 'No start tag found before given position!');
-    Result := AView.Find('<', 0, APosition);
-    if Result = nil then begin
+  AView.Find('<', Result, 0, APosition, 0);
+  if Result.Length = 0 then begin
+    AView.Find('<', Result, 0, APosition);
+    if Result.Length = 0 then begin
       ATagName := '';
       Exit;
     end;
@@ -65,13 +67,11 @@ begin
   //   - if InnerLevel > 0 then InnerLevel := InnerLevel - 1;
   //   - else TagEnd has been found
 
-  //DebugWrite('ExtractTagName', Format('Start of tag: (%d-%d): "%s"', [Tag.Start, Tag.&End, Tag.Text]));
-  TagEnd := AView.Find('>', 0, Result.EndPos + 1);
-  if TagEnd = nil then begin
+  AView.Find('>', TagEnd, 0, Result.EndPos + 1);
+  if TagEnd.Length = 0 then begin
     ATagName := '';
     Exit;
   end else begin
-    //DebugWrite('ExtractTagName', Format('End of tag: (%d-%d): "%s"', [TagEnd.Start, TagEnd.&End, TagEnd.Text]));
     Result.EndPos := TagEnd.EndPos;
     FreeAndNil(TagEnd);
   end;
@@ -82,7 +82,7 @@ begin
   ClosureFound := False;
   StartIndex := 0;
   EndIndex := 0;
-  ATagName := Result.Text;
+  ATagName := UTF8Encode(Result.Text);
   ExtraChar := #0;
   for i := 2 to Length(ATagName) - 1 do begin
     if StartIndex = 0 then begin
@@ -117,7 +117,6 @@ begin
         ClosureFound := False;
       end;
     end;
-    //DebugWrite('ExtractTagName', Format('%d=%s; opens=%d,closes=%d; start=%d,end=%d', [i, ATagName[i], integer(AOpening), integer(AClosing or ClosureFound), StartIndex, EndIndex]));
   end;
   AClosing := AClosing or ClosureFound;
   if EndIndex = 0 then
@@ -181,6 +180,7 @@ begin
   Tags := TStringList.Create;
   MatchingTag := nil;
   NextTag := nil;
+  Found := TTextRange.Create(doc);
   Direction := dirUnknown;
   try
     try
@@ -205,8 +205,6 @@ begin
             end;
           end;
 
-//          DebugWrite('FindMatchingTag', Format('Found TTextRange(%d, %d, "%s"): opens=%d, closes=%d', [Tag.StartPos, Tag.EndPos, Tag.Text, integer(TagOpens), integer(TagCloses)]));
-
           if TagOpens and TagCloses then begin // A self-closing tag
             TagName := '*' + TagName;
 
@@ -225,13 +223,10 @@ begin
 
           end else begin // A tag that doesn't open and doesn't close?!? This should never happen
             TagName := TagName + Format('[opening=%d,closing=%d]', [integer(TagOpens), integer(TagCloses)]);
-//            DebugWrite('FindMatchingTag', Format('%s (%d-%d): "%s"', [TagName, Tag.StartPos, Tag.EndPos, Tag.Text]));
             Assert(False, 'This tag doesn''t open, and doesn''t close either!?! ' + TagName);
             MessageBeep(MB_ICONERROR);
 
           end{if TagOpens and/or TagCloses};
-
-//          DebugWrite('FindMatchingTag', Format('Processed TTextRange(%d, %d, "%s")', [Tag.StartPos, Tag.EndPos, Tag.Text]));
 
         end{if Assigned(Tag)};
 
@@ -240,8 +235,9 @@ begin
         case Direction of
           dirForward: begin
             // look forward for corresponding closing tag
-            NextTag := doc.Find('<[^%\?]', SCFIND_REGEXP or SCFIND_POSIX, Tag.EndPos);
-            if Assigned(NextTag) then
+            NextTag := TTextRange.Create(doc);
+            doc.Find('<[^%\?]', NextTag, SCFIND_REGEXP or SCFIND_POSIX, Tag.EndPos);
+            if NextTag.Length <> 0 then
               NextTag.EndPos := NextTag.EndPos - 1;
           end;
           dirBackward: begin
@@ -252,8 +248,9 @@ begin
             repeat
               if Assigned(NextTag) then
                 NextTag.Free;
-              NextTag := doc.Find('>', 0, InitPos, 0);
-              if Assigned(NextTag) then begin
+              NextTag := TTextRange.Create(doc);
+              doc.Find('>', NextTag, 0, InitPos, 0);
+              if NextTag.Length <> 0 then begin
                 if NextTag.StartPos = 0 then begin
                   FreeAndNil(NextTag);
                   Break;
@@ -263,7 +260,6 @@ begin
                   InitPos := NextTag.StartPos;
                   Continue;
                 end else begin
-//                  OutputDebugString(PChar(Format('npp_htmltag:Found(%d-%d, "%s")', [NextTag.StartPos, NextTag.EndPos, NextTag.Text])));
                   NextTag.StartPos := NextTag.StartPos + 1;
                   Break;
                 end;
@@ -284,9 +280,7 @@ begin
       until (NextTag = nil) or (MatchingTag <> nil);
 
       Tags.LineBreak := #9;
-//      DebugWrite('FindMatchingTag:Done looking', Format('Tags.Count = %d (%s)', [Tags.Count, Tags.Text]));
       if Assigned(MatchingTag) then begin
-//        DebugWrite('FindMatchingTag:Marking', Format('MatchingTag = TTextRange(%d, %d, "%s")', [MatchingTag.StartPos, MatchingTag.EndPos, MatchingTag.Text]));
         if Tags.Count = 2 then begin
           Tag := TTextRange(Tags.Objects[0]);
           if ASelect then begin
@@ -306,21 +300,19 @@ begin
             try
               if AContentsOnly and True then begin // TODO: make optional, read setting from .ini ([MatchTag] SkipWhitespace=1)
                 // Leave out whitespace at begin
-                Found := doc.Find('[^ \r\n\t]', SCFIND_REGEXP or SCFIND_POSIX, Target.StartPos, Target.EndPos);
-                if Assigned(Found) then begin
+                doc.Find('[^ \r\n\t]', Found, SCFIND_REGEXP or SCFIND_POSIX, Target.StartPos, Target.EndPos);
+                if Found.Length <> 0 then begin
                   try
                     Target.StartPos := Found.StartPos;
                   finally
-                    Found.Free;
                   end;
                 end;
                 // Also leave out whitespace at end
-                Found := doc.Find('[^ \r\n\t]', SCFIND_REGEXP or SCFIND_POSIX, Target.EndPos, Target.StartPos);
-                if Assigned(Found) then begin
+                doc.Find('[^ \r\n\t]', Found, SCFIND_REGEXP or SCFIND_POSIX, Target.EndPos, Target.StartPos);
+                if Found.Length <> 0 then begin
                   try
                     Target.EndPos := Found.EndPos;
                   finally
-                    Found.Free;
                   end;
                 end;
               end;
@@ -329,7 +321,6 @@ begin
               Target.Free;
             end;
           end else begin
-//            DebugWrite('FindMatchingTag:Marking', Format('CurrentTag = TTextRange(%d, %d, "%s")', [Tag.StartPos, Tag.EndPos, Tag.Text]));
             MatchingTag.Select;
             {$IFNDEF NPPUNICODE} // NPP Unicode has always done this itself
             if HIWORD(npp.SendMessage(NPPM_GETNPPVERSION)) < 5 then begin
@@ -363,17 +354,17 @@ begin
     except
       on E: Exception do begin
         MessageBeep(MB_ICONERROR);
-//        DebugWrite('FindMatchingTag:Exception', Format('%s: "%s"', [E.ClassName, E.Message]));
       end;
     end;
   finally
     while Tags.Count > 0 do begin
       Tag := TTextRange(Tags.Objects[0]);
-//      DebugWrite('FindMatchingTag:Cleanup', Format('Tags["%s"] = TTextRange(%d, %d, "%s")', [Tags.Strings[0], Tag.StartPos, Tag.EndPos, Tag.Text]));
       Tags.Objects[0].Free;
       Tags.Delete(0);
     end;
     FreeAndNil(Tags);
+    FreeAndNil(Found);
+    FreeAndNil(NextTag);
   end;
 
   //MessageBox(npp.WindowHandle, PChar('Current tag: ' + TagName), scPTitle, MB_ICONINFORMATION);

src/LibNppPlugin/NppSimpleObjects.pas
@@ -42,7 +42,7 @@ interface
         function  GetIndentLevel(): integer;
         procedure SetIndentLevel(const AValue: integer);
       public
-        constructor Create(const AEditor: TActiveDocument; const AStartPos, AEndPos: Sci_Position); overload;
+        constructor Create(const AEditor: TActiveDocument; const AStartPos: Sci_Position = 0; AEndPos: Sci_Position = 0); overload;
         destructor  Destroy; override;
 
         procedure Select();
@@ -157,10 +157,9 @@ interface
         procedure SelectLines(const FirstLine: Sci_Position; const LineCount: Sci_Position = 1);
         procedure SelectColumns(const FirstPosition, LastPosition: Sci_Position);
 
-        function  Find(const AText: WideString; const AOptions: integer = 0;
-                        const AStartPos: Sci_Position = -1; const AEndPos: Sci_Position = -1): TTextRange; overload;
-        function  Find(const AText: WideString; const AOptions: integer;
-                        const ATarget: TTextRange): TTextRange; overload;
+        procedure Find(const AText: WideString; var ATarget: TTextRange; const AOptions: integer = 0;
+                        const AStartPos: Sci_Position = -1; const AEndPos: Sci_Position = -1); overload;
+        procedure Find(const AText: WideString; var ATarget: TTextRange; const AOptions: integer); overload;
 
         property Name: WideString     read GetName;
         property Path: WideString     read GetPath;
@@ -215,14 +214,12 @@ interface
 
         function  GetCount(): cardinal;
         function  GetItem(const AIndex: cardinal): TActiveDocument; overload;
-        function  GetItem(const AName: WideString): TActiveDocument; overload;
       public
         function Open(const Path: WideString): TActiveDocument;
 
         property Count: cardinal                                read GetCount;
 
         property Item[const Index: cardinal]: TActiveDocument   read GetItem; default;
-        property Item[const Name: WideString]: TActiveDocument  read GetItem; {$IFNDEF FPC}default;{$ENDIF}
     end;
 
     { -------------------------------------------------------------------------------------------- }
@@ -262,7 +259,6 @@ implementation
 uses
   {$IFDEF SCI_5}Math,{$ENDIF}
   SysUtils;
-//  L_DebugLogger,
 
 var
   Application: TApplication;
@@ -285,7 +281,6 @@ var
   i: integer;
   trm: TTextRangeMark;
 begin
-//  DebugWrite('NppSimpleObjects.TextRangeUnmarkTimer', 'EventID: ' + IntToStr(AEventID));
   KillTimer(0, AEventID);
   if Assigned(TimedTextRangeMarks) then begin
     for i := 0 to TimedTextRangeMarks.Count - 1 do begin
@@ -301,7 +296,7 @@ end;
 { ================================================================================================ }
 { TTextRange }
 
-constructor TTextRange.Create(const AEditor: TActiveDocument; const AStartPos, AEndPos: Sci_Position);
+constructor TTextRange.Create(const AEditor: TActiveDocument; const AStartPos: Sci_Position = 0; AEndPos: Sci_Position = 0);
 begin
   FEditor := AEditor;
   SetStart(AStartPos);
@@ -389,7 +384,6 @@ var
 begin
   Chars := UTF8Encode(AValue);
   TxtRng := System.Length(Chars);
-//DebugWrite('TTextRange.SetText', Chars);
   FEditor.SendMessage(SCI_SETTARGETSTART, FStartPos);
   FEditor.SendMessage(SCI_SETTARGETEND, FEndPos);
   Dec(FEndPos, (FEndPos - FStartPos) - Integer(FEditor.SendMessage(SCI_REPLACETARGET, TxtRng, PAnsiChar(Chars))));
@@ -566,7 +560,6 @@ begin
   Chars := AnsiString(StringOfChar(#0, Self.GetLength + 1));
   FEditor.SendMessage(SCI_GETSELTEXT, 0, PAnsiChar(Chars));
   Result := WideString(UTF8Decode(Chars));
-//  DebugWrite('TSelection.GetText', Result);
 end;
 { ------------------------------------------------------------------------------------------------ }
 procedure TSelection.SetText(const AValue: WideString);
@@ -576,7 +569,6 @@ var
   Reversed: boolean;
 begin
   Chars := UTF8Encode(AValue);
-//DebugWrite('TSelection.SetText', Chars);
   NewLength := System.Length(Chars) - 1;
   Reversed := (Self.Anchor > Self.GetCurrentPos);
   FEditor.SendMessage(SCI_REPLACESEL, 0, PAnsiChar(Chars));
@@ -1137,18 +1129,14 @@ var
   MsgConst: string;
 begin
   MsgConst := GetMsgConstString(Message, FIsNpp);
-//  DebugWrite('SendMessage', Format('hWnd: %d; Message: %d%s; wParam: %d; lParam: %d', [FWindowHandle, Message, MsgConst, wParam, lParam]));
   try
     if SendMessageTimeout(FWindowHandle, Message, wParam, lParam, SMTO_NORMAL, 5000, @Result) = 0 then
       RaiseLastOSError;
-//    DebugWrite('SendMessage', Format('hWnd: %d; Result: %d', [FWindowHandle, Result]));
   except
     on E: EOSError do begin
-//      DebugWrite('SendMessage', Format('hWnd: %d; Error %d: %s', [FWindowHandle, E.ErrorCode, E.Message]));
       raise;
     end;
     on E: Exception do begin
-//      DebugWrite('SendMessage', Format('hWnd: %d; Error[%s]: %s', [FWindowHandle, E.ClassName, E.Message]));
       raise;
     end;
   end;
@@ -1159,18 +1147,14 @@ var
   MsgConst: string;
 begin
   MsgConst := GetMsgConstString(Message, FIsNpp);
-//  DebugWrite('SendMessage', Format('hWnd: %d; Message: %d%s; wParam: %d; lParam: 0x%p', [FWindowHandle, Message, MsgConst, wParam, lParam]));
   try
     if SendMessageTimeout(FWindowHandle, Message, wParam, Windows.LPARAM(lParam), SMTO_NORMAL, 5000, @Result) = 0 then
       RaiseLastOSError;
-//    DebugWrite('SendMessage', Format('hWnd: %d; Result: %d', [FWindowHandle, Result]));
   except
     on E: EOSError do begin
-//      DebugWrite('SendMessage', Format('hWnd: %d; Error %d: %s', [FWindowHandle, E.ErrorCode, E.Message]));
       raise;
     end;
     on E: Exception do begin
-//      DebugWrite('SendMessage', Format('hWnd: %d; Error[%s]: %s', [FWindowHandle, E.ClassName, E.Message]));
       raise;
     end;
   end;
@@ -1182,18 +1166,14 @@ var
   MsgConst: string;
 begin
   MsgConst := GetMsgConstString(Message, FIsNpp);
-//  DebugWrite('PostMessage', Format('hWnd: %d; Message: %d%s; wParam: %d; lParam: %d', [FWindowHandle, Message, MsgConst, wParam, lParam]));
   try
     if Windows.PostMessage(FWindowHandle, Message, wParam, Windows.LPARAM(lParam)) = False then
       RaiseLastOSError;
-//    DebugWrite('PostMessage', Format('hWnd: %d', [FWindowHandle]));
   except
     on E: EOSError do begin
-//      DebugWrite('PostMessage', Format('hWnd: %d; Error %d: %s', [FWindowHandle, E.ErrorCode, E.Message]));
       raise;
     end;
     on E: Exception do begin
-//      DebugWrite('PostMessage', Format('hWnd: %d; Error[%s]: %s', [FWindowHandle, E.ClassName, E.Message]));
       raise;
     end;
   end;
@@ -1204,18 +1184,14 @@ var
   MsgConst: string;
 begin
   MsgConst := GetMsgConstString(Message, FIsNpp);
-//  DebugWrite('PostMessage', Format('hWnd: %d; Message: %d%s; wParam: %d; lParam: 0x%p', [FWindowHandle, Message, MsgConst, wParam, lParam]));
   try
     if Windows.PostMessage(FWindowHandle, Message, wParam, Windows.LPARAM(lParam)) = False then
       RaiseLastOSError;
-//    DebugWrite('PostMessage', Format('hWnd: %d', [FWindowHandle]));
   except
     on E: EOSError do begin
-//      DebugWrite('PostMessage', Format('hWnd: %d; Error %d: %s', [FWindowHandle, E.ErrorCode, E.Message]));
       raise;
     end;
     on E: Exception do begin
-//      DebugWrite('PostMessage', Format('hWnd: %d; Error[%s]: %s', [FWindowHandle, E.ClassName, E.Message]));
       raise;
     end;
   end;
@@ -1234,15 +1210,18 @@ end;
 
 { ------------------------------------------------------------------------------------------------ }
 
-function TActiveDocument.Find(const AText: WideString; const AOptions: integer; const ATarget: TTextRange): TTextRange;
+procedure TActiveDocument.Find(const AText: WideString; var ATarget: TTextRange; const AOptions: integer);
 begin
-  if Assigned(ATarget) then
-    Result := Find(AText, AOptions, ATarget.StartPos, ATarget.EndPos)
-  else
-    Result := Find(AText, AOptions);
+  if Assigned(ATarget) then 
+  begin
+    if AOptions <> 0 then
+      Find(AText, ATarget, AOptions, ATarget.StartPos, ATarget.EndPos)
+    else
+      Find(AText, ATarget);
+  end;
 end;
 { ------------------------------------------------------------------------------------------------ }
-function TActiveDocument.Find(const AText: WideString; const AOptions: integer; const AStartPos, AEndPos: Sci_Position): TTextRange;
+procedure TActiveDocument.Find(const AText: WideString; var ATarget: TTextRange; const AOptions: integer; const AStartPos, AEndPos: Sci_Position);
 var
   TTF: RSciTextToFind;
   StartPos: LRESULT;
@@ -1260,13 +1239,11 @@ begin
   TTF.chrgText := TTF.chrg;
   StartPos := SendMessage(SCI_FINDTEXT, AOptions, @TTF);
   if StartPos = -1 then begin
-    Result := nil;
-//    DebugWrite(Format('Find("%s"; %d; %d; %d)', [AText, AOptions, AStartPos, AEndPos]),
-//               'No match found');
+    ATarget.SetStart(0);
+    ATarget.SetEnd(0);    
   end else begin
-    Result := TTextRange.Create(Self, TTF.chrgText.cpMin, TTF.chrgText.cpMax);
-//    DebugWrite(Format('Find("%s"; %d; %d; %d)', [AText, AOptions, AStartPos, AEndPos]),
-//               Format('Found match in range %d-%d', [TTF.chrgText.cpMin, TTF.chrgText.cpMax]));
+    ATarget.SetStart(TTF.chrgText.cpMin);
+    ATarget.SetEnd(TTF.chrgText.cpMax);        
   end;
 end;
 
@@ -1588,14 +1565,6 @@ end;
 
 { ------------------------------------------------------------------------------------------------ }
 
-function TDocuments.GetItem(const AName: WideString): TActiveDocument;
-begin
-{$MESSAGE HINT 'TODO: shouldn''t this return a TDocument?'}
-  Result := nil;
-end;
-
-{ ------------------------------------------------------------------------------------------------ }
-
 function TDocuments.Open(const Path: WideString): TActiveDocument;
 begin
 {$MESSAGE HINT 'TODO: open the document'}
@@ -1640,10 +1609,8 @@ function TApplication.GetDocument: TActiveDocument;
 var
   Index: Cardinal;
 begin
-//  DebugWrite('TApplication.GetDocument');
   Self.SendMessage(NPPM_GETCURRENTSCINTILLA, 0, @Index);
   Result := FEditors.Item[Index];
-//  DebugWrite('TApplication.GetDocument', Format('Handle: %d', [Result.FWindowHandle]));
 end;
 
 { ------------------------------------------------------------------------------------------------ }
@@ -1651,7 +1618,7 @@ end;
 function TApplication.GetPath: WideString;
 begin
 {$MESSAGE HINT 'TODO: use GetModuleFileNameEx'}
-  Result := ParamStr(0); // TODO: make long path?
+  Result := UTF8Decode(ParamStr(0)); // TODO: make long path?
 end;
 
 { ------------------------------------------------------------------------------------------------ }
@@ -1711,7 +1678,6 @@ begin
     except
       on E: Exception do begin
         // ignore
-//        DebugWrite('TTextRangeMark.Destroy', Format('%s: "%s"', [E.ClassName, E.Message]));
       end;
     end;
   finally
@@ -1750,23 +1716,18 @@ initialization
 
 finalization
   if Assigned(TimedTextRangeMarks) then begin
-//    DebugWrite('NppSimpleObjects.finalization', 'TimedTextRangeMarks[].Free');
     while TimedTextRangeMarks.Count > 0 do begin
       TRTimerID := TTextRangeMark(TimedTextRangeMarks.Items[0]).FTimerID;
       if TRTimerID <> 0 then
         KillTimer(0, TRTimerID);
-//      DebugWrite('NppSimpleObjects.finalization', 'KillTimer(' + IntToStr(TRTimerID) + ')');
       TTextRangeMark(TimedTextRangeMarks.Items[0]).Free;
       TimedTextRangeMarks.Delete(0);
     end;
-//    DebugWrite('NppSimpleObjects.finalization', 'FreeAndNil(TimedTextRangeMarks)');
     FreeAndNil(TimedTextRangeMarks);
   end;
 
   if Assigned(Application) then begin
-//    DebugWrite('NppSimpleObjects.finalization', 'FreeAndNil(Application)');
     FreeAndNil(Application);
   end;
 
 end.
-

src/U_JSEncode.pas
@@ -74,7 +74,7 @@ begin
     CharCode := Ord(Text[CharIndex]);
     if CharCode > 127 then begin
       Text := Copy(Text, 1, CharIndex - 1)
-              + '\u' + IntToHex(CharCode, 4)
+              + WideFormat('\u%s', [IntToHex(CharCode, 4)])
               + Copy(Text, CharIndex + 1);
       Inc(EntitiesReplaced);
     end;
@@ -113,21 +113,23 @@ begin
   doc := npp.ActiveDocument;
 
   Target := TTextRange.Create(doc, doc.Selection.StartPos, doc.Selection.EndPos);
+  Match := TTextRange.Create(doc);
   try
     repeat
-      Match := doc.Find('\\u[0-9A-F][0-9A-F][0-9A-F][0-9A-F]', SCFIND_REGEXP, Target);
-      if Assigned(Match) then begin
+      doc.Find('\\u[0-9A-F][0-9A-F][0-9A-F][0-9A-F]', Match, SCFIND_REGEXP, Target.StartPos, Target.EndPos);
+      if Match.Length <> 0 then begin
         // Adjust the target already
         Target.StartPos := Match.StartPos + 1;
 
         // replace this match's text by the appropriate Unicode character
-        Match.Text := Char(StrToInt('$' + Copy(Match.Text, 3, 4)));
+        Match.Text := WideChar(StrToInt(Format('$%s', [Copy(Match.Text, 3, 4)])));
 
         Inc(Result);
       end;
-    until not Assigned(Match);
+    until Match.Length = 0;
   finally
     Target.Free;
+    Match.Free;
   end;
 //DebugWrite('DecodeJS', Format('Result: %d replacements', [Result]));
 end{DecodeJS};

commit 5c4de2280fbb6ad67f39139635421856e92969a8

Fix potentially infinite loop in tag finder
 * Restore the end condition for unmatched open tags, regressed in 4fc5a0b

 * Properly handle *all* self-closing tags, even invalid ones like <a/>

src/U_HTMLTagFinder.pas
@@ -85,7 +85,13 @@ begin
   ATagName := UTF8Encode(Result.Text);
   ExtraChar := #0;
   for i := 2 to Length(ATagName) - 1 do begin
-    if StartIndex = 0 then begin
+    // Exit early when it's obviously a self-closing tag
+    if (CompareStr(Copy(ATagName, i), '/>') = 0) then begin
+      AOpening := True;
+      AClosing := True;
+      EndIndex := i - 1;
+      break;
+    end else if StartIndex = 0 then begin
       case ATagName[i] of
         '/': begin
           AOpening := False;
@@ -238,7 +244,8 @@ begin
             NextTag := TTextRange.Create(doc);
             doc.Find('<[^%\?]', NextTag, SCFIND_REGEXP or SCFIND_POSIX, Tag.EndPos);
             if NextTag.Length <> 0 then
-              NextTag.EndPos := NextTag.EndPos - 1;
+              NextTag.EndPos := NextTag.EndPos - 1
+            else FreeAndNil(NextTag);
           end;
           dirBackward: begin
             // look backward for corresponding opening tag
@@ -263,7 +270,7 @@ begin
                   NextTag.StartPos := NextTag.StartPos + 1;
                   Break;
                 end;
-              end;
+              end else FreeAndNil(NextTag);
             until not Assigned(NextTag);
 
           end;