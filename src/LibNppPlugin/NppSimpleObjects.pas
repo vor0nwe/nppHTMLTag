vor0nwe/nppHTMLTag

commit afd7989895c6748b553fc27cc6e0c20ea8c2037e

Don't shift caret when clearing selections
Prevents line jumping when a decoded entity is by itself at the
start or end of a line

src/LibNppPlugin/NppSimpleObjects.pas
@@ -484,14 +484,8 @@ end;
 
 { ------------------------------------------------------------------------------------------------ }
 procedure TTextRange.ClearSelection();
-var
-  SciMsg: Cardinal;
 begin
-  if (Self.Anchor > FEditor.CurrentPosition) then
-    SciMsg := SCI_POSITIONBEFORE
-  else
-    SciMsg := SCI_POSITIONAFTER;
-  FEditor.CurrentPosition := FEditor.SendMessage(SciMsg, FEditor.CurrentPosition);
+  Self.Anchor := FEditor.CurrentPosition;
 end;
 
 { ================================================================================================ }

commit 7276d701d0063f51c8979552f1b954d0cf54ac7d

Avoid splitting CRLFs after entity replacement
Also restores caret movement when nothing is replaced,
regressed in afd7989

src/LibNppPlugin/NppSimpleObjects.pas
@@ -550,11 +550,11 @@ function TSelection.GetText: WideString;
 var
   Chars: AnsiString;
 begin
-  Chars := AnsiString(StringOfChar(#0, Self.GetLength + 1));
+  Chars := StringOfChar(#0, Self.GetLength + 1);
   FEditor.SendMessage(SCI_GETSELTEXT, 0, PAnsiChar(Chars));
   case FEditor.SendMessage(SCI_GETCODEPAGE) of
     SC_CP_UTF8:
-      Result := WideString(UTF8Decode(Chars))
+      Result := UTF8Decode(Chars)
     else
       Result := WideString(UTF8Encode(Chars))
   end;
@@ -954,7 +954,12 @@ begin
   Chars := EmptyStr;
   SetLength(Chars, Len);
   SendMessage(SCI_GETTEXT, Len, PAnsiChar(Chars));
-  Result := WideString(Chars);
+  case SendMessage(SCI_GETCODEPAGE) of
+    SC_CP_UTF8:
+      Result := UTF8Decode(Chars)
+    else
+      Result := WideString(UTF8Encode(Chars))
+  end;
 end;
 
 { ------------------------------------------------------------------------------------------------ }

src/U_Npp_HTMLTag.pas
@@ -499,7 +499,7 @@ type
   TReplaceFunc = function(Scope: TEntityReplacementScope = ersSelection): Integer;
 var
   doc: TActiveDocument;
-  anchor, caret: Sci_Position;
+  anchor, caret, nextCaretPos: Sci_Position;
   ch, charOffset: Integer;
   didReplace: Boolean;
 
@@ -555,14 +555,23 @@ begin
     if (ch in [$0A, $0D]) then // ENTER was pressed
       doc.CurrentPosition := doc.NextLineStartPosition
     else begin
+      nextCaretPos := doc.SendMessage(SCI_POSITIONAFTER, doc.CurrentPosition);
+      // stay in current line if at EOL
+      if (nextCaretPos >= doc.NextLineStartPosition) then
+        Exit;
       // no inserted char, nothing to offset
       if (Cmd > dcAuto) then charOffset := -1;
-      doc.CurrentPosition := doc.SendMessage(SCI_POSITIONAFTER, doc.CurrentPosition) + charOffset;
+      doc.CurrentPosition := nextCaretPos + charOffset;
     end;
   end else begin
     // place caret after inserted char
-    if (Cmd = dcAuto) then Inc(caret);
+    if (Cmd = dcAuto) then begin
+      Inc(caret);
+      if (ch = $0A) and (doc.SendMessage(SCI_GETEOLMODE) = SC_EOL_CRLF) then
+        Inc(caret);
+    end;
     doc.Selection.ClearSelection;
+    doc.CurrentPosition := caret;
   end;
 end;
 